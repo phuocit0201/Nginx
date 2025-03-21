45ea4381bc605473d460a37384ca0acb01475f46
PhuocLH <huuphuocit0201@gmail.com>
2025-03-21 14:29:51 +0700

    test cd/cd

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', function () {
 
 Route::get('/send-email-queue', [App\Http\Controllers\SendEmailQueue::class, 'index']);
 Route::post('/send-email-queue', [App\Http\Controllers\SendEmailQueue::class, 'sendEmail'])->name('send.email.queue');
+
+Route::get('/test-cicd', [App\Http\Controllers\SendEmailQueue::class, 'index']);
