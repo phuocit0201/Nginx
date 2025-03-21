dd2969dca59649249959f6442de473fc0735f2d6
PhuocLH <huuphuocit0201@gmail.com>
2025-03-21 15:00:06 +0700

    test ci/cd 3

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::post('/send-email-queue', [App\Http\Controllers\SendEmailQueue::class, 's
 Route::get('/test-cicd', [App\Http\Controllers\SendEmailQueue::class, 'index']);
 Route::get('/test-cicd1', [App\Http\Controllers\SendEmailQueue::class, 'index']);
 Route::get('/test-cicd2', [App\Http\Controllers\SendEmailQueue::class, 'index']);
+Route::get('/test-cicd3', [App\Http\Controllers\SendEmailQueue::class, 'index']);
